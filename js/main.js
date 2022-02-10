18746b69f2cf12f86f880a7c251c18de0d10e014
marcin1989 <marcinlubanski23@gmail.com>
2022-02-10 12:50:23 +0100

    cos tu brakuje

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let first = "Uwielbiam JavaScript";
+
+let second = "Jestem świetnym programistą";
+
+
+function longest () {
+    if (first.length > second.length) {
+        console.log(first);
+    } else (first.length < second.length) {
+        console.log(second);
+    }
+}
